e2be3b0499e67935907a1485a7156ae3aae5bba3
mikeehinger <michaelehinger@Michaels-MacBook-Air-2.local>
2020-03-24 18:41:37 -0400

    try this

--- public/clientJQ.js
+++ public/clientJQ.js
@@ -1,6 +1,9 @@
 //var socket = io('http://localhost:3000');
 //const PORT = process.env.PORT || 3000;
-var socket = io(window.location.hostname+":3000");
+//var socket = io(window.location.hostname+":3000");
+//script(src='./socket.io/socket.io.js')
+var socket = io.connect(window.location.hostname);
+
 
 var gameData;
 var seats = [];
