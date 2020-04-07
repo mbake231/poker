7eb6396a83c982719bae73352188aa55c59d84bb
mikeehinger <mehinger@gmail.com>
2020-04-07 16:11:40 -0400

    fix login

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 
-//const production  = 'https://fartmanjack.herokuapp.com:'+process.env.PORT ;
-const production  = '';
+const production  = BASE_URL+':'+process.env.PORT;
+//const production  = '';
 
 const development = 'http://localhost:3000';
 const url = (process.env.NODE_ENV=='production' ? production : development);
