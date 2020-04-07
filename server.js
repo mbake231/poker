c2bbd54a8297bbf792e71804f520ef5afc65e4b9
mikeehinger <mehinger@gmail.com>
2020-04-07 16:31:06 -0400

    fix login

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 //app.use(cors());
 if(process.env.NODE_ENV === 'production') {
-	app.use(cors({origin: 'https://localhost:3000',credentials: true}));
+	app.use(cors({origin: process.env.BASE_URL+':'+process.env.PORT,credentials: true}));
 }
 else {
 	app.use(cors({origin: 'http://localhost:8000',credentials: true}));
