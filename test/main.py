c3f406d6f90436988eeca82b89ec88f5723451de
prolorenzo <prolorenzo.a@gmail.com>
2020-04-27 22:31:52 +0300

    Some fix in code

--- test/main.py
+++ test/main.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 from subprocess import Popen
 import glob
 
 tests = glob.glob('test*.py')
 processes = []
 for test in tests:
-    processes.append(Popen('python %s' % test, shell=True))
+    processes.append(Popen('python3 %s' % test, shell=True))
 
 for process in processes:
     process.wait()
